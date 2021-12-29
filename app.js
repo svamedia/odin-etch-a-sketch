8a5b0ef57b01297fcab209e49674958e7719fdf3
Sebastian <svamediamarketing@gmail.com>
2021-12-28 19:09:15 -0500

    dynamcally creates 10x10 grid when createGrid(); is called.

--- app.js
+++ app.js
@@ -2,15 +2,27 @@
 // Grabs html div element and assigns it to variable
 let gridContainer_div = document.querySelector(".grid-container");
 
-// creation of new div
-let grid_table = document.createElement("table");
-// appends grid_table to html div element
-gridContainer_div.appendChild(grid_table);
-grid_table.id = "grid-table";
+// function that contains for loop to create table rows to specific value
 
-// create table row element
-let myRow = document.createElement("tr");
-grid_table.appendChild(myRow);
+function createGrid() {
+  // creation of new div
+  let grid_table = document.createElement("table");
 
-let rowCell = document.createElement("td");
-myRow.appendChild(rowCell);
+  // appends grid_table to html div element
+  gridContainer_div.appendChild(grid_table);
+  grid_table.id = "grid-table";
+
+  for (let i = 0; i < 11; i++) {
+    // create table row element
+    let myRow = document.createElement("tr");
+    myRow.id = "row" + i;
+
+    grid_table.appendChild(myRow);
+
+    for (let j = 0; j < 11; j++) {
+      let rowCell = document.createElement("td");
+      myRow.appendChild(rowCell);
+      rowCell.classList.add("table-cell");
+    }
+  }
+}
