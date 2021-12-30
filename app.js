88839176335bf5fa2421164473a945669d5d3e49
Sebastian <svamediamarketing@gmail.com>
2021-12-29 19:48:03 -0500

    created a nested function that runs onmouseover event & changes rowCell bg

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // cache the DOM
 // Grabs html div element and assigns it to variable
 let gridContainer_div = document.querySelector(".grid-container");
-
 // function that contains for loop to create table rows to specific value
 
 function createGrid() {
@@ -23,8 +22,14 @@ function createGrid() {
       let rowCell = document.createElement("td");
       myRow.appendChild(rowCell);
       rowCell.classList.add("table-cell");
+      // nested function that adds the css styles via dom manipulation and css
+      rowCell.onmouseover = function changeCellColor() {
+        rowCell.style.backgroundColor = "red";
+      };
     }
   }
 }
+
+//event listener changes the background color when mouse is hovered over
 
 window.addEventListener("load", createGrid());
