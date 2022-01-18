260e9a0af20360cfc0d2c901094ea29c5b615853
Sebastian <svamediamarketing@gmail.com>
2022-01-17 19:59:40 -0500

    user can now set and input canvas size

--- app.js
+++ app.js
@@ -6,19 +6,21 @@ let gridContainer_div = document.querySelector(".grid-container");
 function createGrid() {
   // creation of new div
   let grid_table = document.createElement("table");
+  let heights = document.getElementById("inputHeight").value;
+  let widths = document.getElementById("inputWidth").value;
 
   // appends grid_table to html div element
   gridContainer_div.appendChild(grid_table);
   grid_table.id = "grid-table";
 
-  for (let i = 0; i < 17; i++) {
+  for (let i = 0; i < heights; i++) {
     // create table row element
     let myRow = document.createElement("tr");
     myRow.id = "row" + i;
 
     grid_table.appendChild(myRow);
 
-    for (let j = 0; j < 17; j++) {
+    for (let j = 0; j < widths; j++) {
       let rowCell = document.createElement("td");
       myRow.appendChild(rowCell);
       rowCell.classList.add("table-cell");
@@ -31,4 +33,4 @@ function createGrid() {
 }
 
 //event listener changes the background color when mouse is hovered over
-window.addEventListener("load", createGrid());
+// window.addEventListener("load", createGrid());
